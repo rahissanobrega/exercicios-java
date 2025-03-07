9422bb8fa6cda6fdde0eab7be5c3ab2db5571899
Rafael Nobrega <rafael.jesus.nobrega@gmail.com>
2025-03-07 18:43:56 -0300

    Primeira Alteração

---
+++ src/parOuImpar/ParOuImpar.java
@@ -0,0 +1,27 @@
+package parOuImpar;
+
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int numero = scanner.nextInt();
+        scanner.close();
+        System.out.println("--------------------------");
+
+        if (numero % 2 == 0 && numero >= 0) {
+            System.out.println("O numero " + numero + " é par e positivo");
+        } else if (numero % 2 != 0 && numero > 0) {
+            System.out.println("O numero " + numero + " é impar e positivo");
+        }else if (numero % 2 == 0 && numero <= 0) {
+            System.out.println("O numero " + numero + " é par e negativo");
+        }else {
+            System.out.println("O numero " + numero + " é impar e negativo");
+        }
+
+
+    }
+}
