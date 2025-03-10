5f951602b07201a52c78ee5dad79e80d516fa0d8
Rafael Nobrega <rafael.jesus.nobrega@gmail.com>
2025-03-10 15:06:15 -0300

    Inclusão do exercício maior idade

---
+++ src/exercicios/MaiorIdade.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class MaiorIdade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nEscreva seu nome: ");
+        String nomePessoa = scanner.next();
+        System.out.println("\nInforme sua idade: ");
+        int idadePessoa = scanner.nextInt();
+
+        if (idadePessoa >= 18){
+            System.out.printf("%s, você é maior de idade, portanto pode comprar bebidas alcoólicas", nomePessoa);
+        }else {
+            System.out.printf("%s, você é menor de idade, portanto não pode comprar bebidas alcoólicas", nomePessoa);
+        }
+
+
+    }
+}
