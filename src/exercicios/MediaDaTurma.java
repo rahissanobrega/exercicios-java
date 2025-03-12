4bb38be86f8b4ce9888b3c98023017b15403d014
Rafael Nobrega <rafael.jesus.nobrega@gmail.com>
2025-03-12 13:48:37 -0300

    Calculo medio de notas na turma

---
+++ src/exercicios/MediaDaTurma.java
@@ -0,0 +1,31 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class MediaDaTurma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\n       Cálculo de média de notas na turma");
+        System.out.println("------------------------------------------------------");
+
+        int quantidadeDeNotas = 0;
+        double nota = 0;
+        double total = 0;
+
+        while (nota != -1) {
+            System.out.println("Informe a nota (ou digite -1 para obter o resultado) ");
+            nota = scanner.nextDouble();
+            if (nota <= 10 && nota >= 0) {
+                total += nota;
+                quantidadeDeNotas++;
+            } else {
+                System.out.println("informe uma nota válida!!!!");
+            }
+        }
+        double media = total / quantidadeDeNotas;
+        System.out.printf("A média da turma ficou de %.2f", media);
+        System.out.println();
+        scanner.close();
+    }
+}
